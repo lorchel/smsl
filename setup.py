d6635d83a1b89a32a16650ca4e0e5766012b1a0d
lorchel <lorchel@gmx.de>
2016-07-01 11:48:33 +0200

    refactor and switch to more flexible configuration in JSON format

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-VERSION = '0.6'
+VERSION = '0.7'
 with open('README.rst') as f:
     README = f.read()
     DESCRIPTION = README.split('\n')[2]
--- smsl.py
+++ smsl.py
@@ -1,82 +1,71 @@
 #!/usr/bin/env python
 """
 # SMSL
-### Tool for sending SMS via HTML SMSlink
+### Send command-line SMS via HTML API
 
 This is a command line utility for sending short messages with the help of
-HTML SMSlink and different providers. Now sending a SMS is as far as typing:
+the HTML API of different providers. Now sending a SMS is as far as typing:
 
     send dude "Hey Dude!"
 """
 
+import argparse
+import csv
+import json
+import os
+import re
+
 try:
-    import configparser
-    from configparser import ConfigParser as ConfigParser
     from urllib.request import urlopen
     TRANS = (str.maketrans(str.maketrans('', '', ' -()')),)
 except ImportError:
-    import ConfigParser as configparser
-    from ConfigParser import SafeConfigParser as ConfigParser
     from urllib import urlopen
     TRANS = (None, ' -()')
-import argparse
-import csv
-import getpass
-import os
-import sys
 
-
-CONFIG_FILENAME = os.path.expanduser(os.path.join('~', '.config', 'smsl.cfg'))
-CONFIG_FILENAME_ALT = os.path.expanduser(os.path.join('~', '.smsl.cfg'))
+CONFIG = os.path.expanduser(os.path.join('~', '.config', 'smsl.json'))
 EPILOG = """
 At the first start an example configuration file at the path
-%s will be created. Feel free to adapt the file to your needs.
-You can add new contacts, new users and a csv file which will additionally
-be searched for contacts. All contacts are shared between the users.
-You can add your providers urls (one example url is provided), password and
-caller information, so that you don't need to enter it every time you want to
-send a short message.
+%s will be created which must be adapted.
+You can add profiles, contacts, and a csv file which will additionally
+be searched for contacts. All contacts are shared between the profiles.
 
-If you don't want your password to be saved on your harddisk in plain letters
-you can comment out this option and enter it each time you send a SMS.
-Anyway the created link will include your password in plain letters
-and it will be send over your internet connection. This means don't use an
-expensive password on your providers account when using this tool.
-
-By the way you need to be registered at your providers website and you need to
-have some money on your account. The tool uses the HTML API.
-The script creates a link like
-https://www.smslisto.com/myaccount/sendsms.php?username=xxx&password=xxx&
-from=xxx&to=xxx&text=xxx
-and sends it to the provider.
+You need to be registered at a provider which provides the HTML API. The tool
+creates a link like https://www.x.com/sendsms?user={user}&pw={pw}&to={to}&
+from={from}&text={text} which will be sent to the provider.
 
 Give your thumb a break! ;)
-""" % CONFIG_FILENAME
+""" % CONFIG
 
 CONFIG_EXAMPLE = """
-# Commented lines start with a '#'.
+# Comments start with #.
 # Please edit the file according to your preferences.
 
-[Settings]
-default_user = example_user
-#url = https://www.smslisto.com/myaccount/sendsms.php?username={user}&password={pw}&from={caller}&to={to}&text={text}
-country = +1
-#history = ~/.config/smsl_hist.txt # uncomment for history of sent sms
+{
+"default_profile": "example",
 
-[Contacts]
-dude = +1234567890
+#"contacts": {
+#    "dude": "+1234567890"
+#    },
 
-[ContactsCSV]
-# Add the full path to your csv file if you want to search there for mobile
-# numbers. Column names have to be in the first row in this file.
-#file =
-#colreceiver = column header for 'name of receiver' column
-#colnumber = column header for 'number of receiver' column
+#"contacts_csv": {
+#    "file": "example.csv",
+#    "colreceiver": "name",  # column name for receiver names
+#    "colnumber": "mobile"   # column name for mobile number
+#    },
 
-[example_user]
-username = Your provider username
-#password = Your provider password
-from = Your username or your verified phone number
+"example": {
+    # url for HHTP request of provider SMS gateway, {} fields are replaced by the corresponding variables,
+    # must contain mandatory {to} and {text} fields
+    "url": "https://www.x.com/sendsms?user={user}&pw={pw}&to={to}&from={from}&text={text}",
+#    "print_answer": true,  # print answer from server
+#    "history": null,       # write history of all sent sms in this file
+#    "country_code": null,  # replace leading zero with country code, e.g. "+1"
+    # default parameters for {} fields in url, can be changed from command line
+    "user": "your_username",
+    "pw": "your_password",
+    "from": "your_phone_number"
+    }
+}
 """
 
 
@@ -84,9 +73,71 @@ class SmslError(Exception):
     pass
 
 
-def send_sms(url, text, user=None, pw=None, caller=None, to=None, test=False):
+class ConfigJSONDecoder(json.JSONDecoder):
+    """Strip lines from comments."""
+
+    def decode(self, s):
+        s = '\n'.join(l.split('#', 1)[0] for l in s.split('\n'))
+        return super(ConfigJSONDecoder, self).decode(s)
+
+
+def read_config(fname):
+    if not os.path.exists(fname):
+        dirname = os.path.dirname(fname)
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+        with open(fname, 'wb') as f:
+            f.write(CONFIG_EXAMPLE.strip('\n'))
+    try:
+        with open(fname) as f:
+            return json.load(f, cls=ConfigJSONDecoder)
+    except ValueError as ex:
+        raise SmslError('Error while parsing the configuration: %s' % ex)
+
+
+def get_contacts(config):
+    contacts = config.get('contacts', {})
+    try:
+        subconf = config['contacts_csv']
+        database = os.path.expanduser(subconf['file'])
+        colreceiver = subconf['colreceiver'].strip()
+        colreceiver2 = subconf.get('colreceiver2', '').strip()
+        colnumber = subconf['colnumber'].strip()
+    except KeyError:
+        return contacts
+    try:
+        with open(database, 'r') as f:
+            reader = csv.DictReader(f, skipinitialspace=True)
+            contacts2 = {row[colreceiver]: row[colnumber]
+                         for row in reader if row[colreceiver]}
+            contacts2.update(contacts)
+            if colreceiver2:
+                contacts2.update({row[colreceiver2]: row[colnumber]
+                                  for row in reader if row[colreceiver2]})
+            return contacts2
+    except IOError:
+        raise SmslError('CSV file does not exist at %s.' % database)
+    except (csv.Error, KeyError):
+        raise SmslError('Error while parsing the CSV file %s.' %
+                        database)
+
+
+def transform_number(number, contacts, country_code=None):
+    onumber = number
+    number = number.translate(*TRANS)
+    if number in contacts:
+        number = contacts[number]
+    if number.startswith('0') and country_code is not None:
+        number = number.replace('0', country_code, 1)
+    if not (number.startswith('+') and number[1:].isdigit() or
+            number.isdigit()):
+        raise SmslError('%s is not a valid phone number' % onumber)
+    return number
+
+
+def send_sms(url, text, to, test=False, **kwargs):
     """Send SMS with a HTML SMSlink."""
-    url = url.format(text=text, user=user, pw=pw, caller=caller, to=to)
+    url = url.format(text=text, to=to, **kwargs)
     if test:
         answer = 'Constructed url: %s' % url
     else:
@@ -96,135 +147,27 @@ def send_sms(url, text, user=None, pw=None, caller=None, to=None, test=False):
     return answer
 
 
-def get_config():
-    """Read config from file or create example config file"""
-    config = ConfigParser()
-    if not config.read([CONFIG_FILENAME, CONFIG_FILENAME_ALT]):
-        dirname = os.path.dirname(CONFIG_FILENAME)
-        if not os.path.exists(dirname):
-            os.mkdir(dirname)
-        with open(CONFIG_FILENAME, 'wb') as f:
-            f.write(CONFIG_EXAMPLE.strip('\n'))
-    return config
-
-
-def is_phone_number(phone, accept_zero=False):
-    """Return if phone is a valid phone number."""
-    p = phone.translate(*TRANS)
-    return (p.startswith('+') and p[1:].isdigit() or
-            p[0] == '0' and p.isdigit() and accept_zero)
-
-
-def read_csv(config, to=None, country=None):
-    if (config.has_option('ContactsCSV', 'file') and
-            config.has_option('ContactsCSV', 'colreceiver') and
-            config.has_option('ContactsCSV', 'colnumber')):
-        database = os.path.expanduser(config.get('ContactsCSV', 'file'))
-        colreceiver = config.get('ContactsCSV', 'colreceiver').strip()
-        colreceiver2 = (config.get('ContactsCSV', 'colreceiver2').strip() if
-                        config.has_option('ContactsCSV', 'colreceiver2')
-                        else None)
-        colnumber = config.get('ContactsCSV', 'colnumber').strip()
-        try:
-            with open(database, 'r') as f:
-                reader = csv.DictReader(f, skipinitialspace=True)
-                if to:
-                    for row in reader:
-                        if (row[colreceiver].strip().lower() == to.lower() or
-                                colreceiver2 and colreceiver2.lower() and
-                                row[colreceiver2].strip().lower() == to):
-                            to = row[colnumber]
-                            if not is_phone_number(to, country):
-                                raise SmslError('Wrong format of number in CSV'
-                                                ' col %s.' % colnumber)
-                            break
-                else:
-                    to = [(row[colreceiver], row[colnumber])
-                          for row in reader if row[colreceiver]]
-                    if colreceiver2:
-                        to.extend([(row[colreceiver2], row[colnumber])
-                                   for row in reader if row[colreceiver2]])
-        except IOError:
-            raise SmslError('CSV file does not exist at %s.' % database)
-        except (csv.Error, KeyError):
-            raise SmslError('Error while parsing the CSV file %s.' %
-                            database)
-    return to
-
-
-def get_all_contacts(config):
-    return sorted((list(config.items('Contacts'))
-                   if config.has_section('Contacts') else []) +
-                  read_csv(config) or [])
-
-
-def get_send_args(config, to, text, test=False, default_user=None):
-    """Get arguments from config or args and raise SmslError if necessary."""
-    if not text:
-        raise SmslError('Your message text is empty.')
-    if not default_user and not config.has_option('Settings', 'default_user'):
-        raise SmslError("The user has to be given as argument with -i "
-                        "or has to be defined "
-                        "in the config file as option default_user in section "
-                        "Settings.")
-    default_user = default_user or config.get('Settings', 'default_user')
-
-    def get_option(option, raw=False):
-        try:
-            return (config.get(default_user, option, raw=raw) if
-                    config.has_option(default_user, option) else
-                    config.get('Settings', option, raw=raw))
-        except (configparser.NoSectionError, configparser.NoOptionError):
-            raise SmslError('Error when reading %s from config file.' % option)
-    user = get_option('username')
-    caller = get_option('from')
-    url = get_option('url')
-    if '?' not in url:
-        raise SmslError('No valid request url: %s' % url)
-    # Get phone number
-    country = get_option('country')
-    # Try to find receiver and number in Contacts and csv file
-    if not is_phone_number(to, country) and config.has_option('Contacts', to):
-        to = config.get('Contacts', to)
-    if not is_phone_number(to, country):
-        to = read_csv(config, to, country)
-    if not is_phone_number(to, country):
-        raise SmslError('Receiver is no valid phone number or contact not '
-                        'found in config file or CSV file.')
-    to = to.translate(*TRANS)
-    msg = ''
-    if to[0] == '0':
-        to = to.replace('0', country, 1)
-        msg = 'Replace 0 by country code %s.' % country
-    try:
-        pw = get_option('password')
-    except SmslError:
-        pw = getpass.getpass('Please enter your provider password: ')
-    kw = dict(url=url, user=user, pw=pw, caller=caller, to=to, text=text,
-              test=test)
-    return kw, msg
-
-
 def main():
-    """Get command line arguments and send sms"""
-    config = get_config()
-    default_user = (config.get('Settings', 'default_user') if
-                    config.has_option('Settings', 'default_user') else None)
+    """Get command line arguments read config and send sms"""
+    config = read_config(CONFIG)
+    default_profile = config.get('default_profile')
     description = __doc__
     parser = argparse.ArgumentParser(
         description=description, epilog=EPILOG,
         formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument('to', nargs='?',
-                        help='Number or contact you wish to send the sms to.')
-    parser.add_argument('text', nargs='*',
-                        help='The message text you want to send. Use quotes! '
-                        'Otherwise there will be errors when using *? and '
-                        'other special characters.')
-    parser.add_argument('-i', '--id', default=default_user,
-                        help='Your alias in the config file '
-                        '(default: %s)' % default_user if default_user else
-                        '(no default)')
+    msg = 'Number or contact you wish to send the sms to.'
+    parser.add_argument('to', nargs='?', help=msg)
+    msg = ('The message text you want to send. Use quotes! Otherwise there '
+           'will be errors when using *? and other special characters.')
+    parser.add_argument('text', nargs='*', help=msg)
+    msg = 'Select the profile'
+    if default_profile is not None:
+        msg = msg + ' (default: %s)' % default_profile
+    msg = msg + '.'
+    parser.add_argument('-p', '--profile', default=default_profile, help=msg)
+    parser.add_argument('-o', '--options', action='store_true',
+                        help='Show availlable options for profile.')
     parser.add_argument('-t', '--test', action='store_true',
                         help='Just print url, do not send message. '
                         'You can copy+paste the url into your webbrowser.')
@@ -232,7 +175,8 @@ def main():
                         help='Count characters in message, do not send.')
     parser.add_argument('-s', '--show', action='store_true',
                         help='Show all available contacts.')
-    args = parser.parse_args()
+
+    args, remaining_args = parser.parse_known_args()
     text = ' '.join(args.text)
     if args.count:
         N = len(text)
@@ -240,28 +184,41 @@ def main():
               'characters' % (N, 1 if N <= 160 else (N - 160 - 1) // 145 + 2))
         return
     if args.show:
-        contacts = get_all_contacts(config)
+        contacts = get_contacts(config)
         if contacts:
             print('%30s %s' % ('contact', 'number'))
             print('%30s %s' % ('-' * 10, '-' * 10))
-            for (name, number) in contacts:
+            for (name, number) in sorted(contacts.items()):
                 print('%30s %s' % (name, number))
         else:
             print('No contacts found.')
         return
-    try:
-        send_kwargs, msg = get_send_args(
-            config, args.to, text, args.test, args.id)
-        if msg:
-            print(msg)
-    except SmslError as ex:
-        sys.exit(ex)
-    answer = send_sms(**send_kwargs)
-    print(answer)
-    if not args.test and config.has_option('Settings', 'history'):
-        log_msg = ("user: %s receiver: %s msg: '%s' response: %s\n" %
-                   (args.id, args.to, text, answer))
-        fname = os.path.expanduser(config.get('Settings', 'history'))
+    prof = config[args.profile]
+    url = prof['url']
+    msg = ('additional arguments according to url in selected profile\n'
+           'url: %s' % url)
+    parser2 = argparse.ArgumentParser(usage=msg, add_help=False)
+    for option in re.findall('\{(.*?)\}', url):
+        if option in ('to', 'text'):
+            continue
+        msg = ('defaults to %s' % prof[option] if option in prof else
+               'mandatory argument')
+        parser2.add_argument('--' + option, default=prof.get(option), help=msg)
+    if args.options:
+        parser2.print_help()
+        return
+    contacts = get_contacts(config)
+    numbers = [transform_number(n, contacts, prof.get('country_code'))
+               for n in args.to.split(',')]
+    to = ','.join(numbers)
+    options = parser2.parse_args(remaining_args)
+    answer = send_sms(url, text, to, test=args.text, **vars(options))
+    if prof.get('print_answer', True) or args.test:
+        print(answer)
+    if not args.test and prof.get('history'):
+        log_msg = ("profile: %s receiver: %s msg: '%s' response: %s\n" %
+                   (args.profile, to, text, answer))
+        fname = os.path.expanduser(prof['history'])
         with open(fname, 'a') as f:
             f.write(log_msg)
 
